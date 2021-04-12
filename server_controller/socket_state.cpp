2a613a32b9bbc47a043d0cde978ef3d899d5e747
jonny p <jpalactor@gmail.com>
2021-04-12 23:58:03 +0000

    k tried to add more code

--- server_controller/socket_state.cpp
+++ server_controller/socket_state.cpp
@@ -9,23 +9,30 @@ using namespace boost::asio;
 
 namespace networking_util
 {
+    // ! Fix comments
     class socket_state
     {
         friend class networking;
 
         public:
+            // member variables
             const tcp::socket the_socket;
             const int buffer_size;
-
             const long id;
-            std::function<socket_state> on_network_action;
 
-            std::string error_message;
-            bool error_occured;
+            // methods
+            socket_state();
+            std::function<socket_state> on_network_action;
+            std::string get_error_message;
+            bool get_error_occured;
+            void remove_data(int start, int length);
+            void clear_data();
 
         private:
+            // member variables
             std::byte[] buffer;
             std::basic_stringstream data;
+            std::string error_message;
 
             static long next_id;
             std::mutex data_mutex;
