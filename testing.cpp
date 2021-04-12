27cb46156fdc9c133ea4cb9b419416ddd118b2d2
Jonny P <jpalactor@gmail.com>
2021-04-12 09:21:39 +0000

    Fake code for server

--- testing.cpp
+++ testing.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <boost/asio.hpp>
 
-int main()
-{
-  std::cout << "yo mama" << std::endl;
-  return 0;
+using namespace boost::asio;
+using ip::tcp;
+using std::string;
+using std::cout;
+using std::endl;
+string read_(tcp::socket & socket) {
+       boost::asio::streambuf buf;
+       boost::asio::read_until( socket, buf, "\n" );
+       string data = boost::asio::buffercast<const char*>(buf.data());
+       return data;
+}
+void send(tcp::socket & socket, const string& message) {
+       const string msg = message + "\n";
+       boost::asio::write( socket, boost::asio::buffer(message) );
+}
+
+int main() {
+      boost::asio::io_service ioservice;
+//listen for new connection
+      tcp::acceptor acceptor(ioservice, tcp::endpoint(tcp::v4(), 1234 ));
+//socket creation 
+      tcp::socket socket(ioservice);
+//waiting for connection
+      acceptor.accept(socket);
+//read operation
+      string message = read(socket);
+      cout << message << endl;
+//write operation
+      send(socket_, "Hello From Server!");
+      cout << "Servent sent Hello message to Client!" << endl;
+   return 0;
 }
