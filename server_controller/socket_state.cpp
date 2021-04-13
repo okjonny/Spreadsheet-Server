56cd94b638e33b38a4cf2280744584ef08e67790
jpalactor@gmail.com <jpalactor@gmail.com>
2021-04-13 05:11:57 +0000

    Compiles but no locks yet.

--- server_controller/socket_state.cpp
+++ server_controller/socket_state.cpp
@@ -1,29 +1,33 @@
 #include "socket_state.h"
-#include <boost/filesystem/operations.hpp>
-#include <boost/thread/mutex.hpp>
+//#include <boost/filesystem/operations.hpp>
+//#include <boost/thread/mutex.hpp>
 #include <boost/asio.hpp>
 #include <mutex>
 
-using namespace boost::asio;
+//using namespace boost::asio;
+//using ip::tcp;
 
 namespace networking_util
 {
-    the_socket = null;
-    next_id = 0;
+    //the_socket = null;
+    //next_id = 0;
 
-    socket_state::socket_state(std::function<networking_util::socket_state> to_call, ip::tcp::socket s)
+    //std::function<networking_util::socket_state()> &to_call,
+
+    socket_state::socket_state(std::function<networking_util::socket_state()> &to_call, boost::asio::ip::tcp::socket &s)
     {
         on_network_action = to_call;
-        the_socket = s;
+        the_socket = &s;
+        next_id = 0;
         id = next_id++;
     }
 
-    std::string socket_state::get_error_message()
+    std::string socket_state::get_error_message() const
     {
         return error_message;
     }
 
-    bool socket_state::get_error_occured()
+    bool socket_state::get_error_occured() const
     {
         return error_occured;
     }
--- server_controller/socket_state.h
+++ server_controller/socket_state.h
@@ -1,5 +1,5 @@
-#include <boost/filesystem/operations.hpp>
-#include <boost/thread/mutex.hpp>
+//#include <boost/filesystem/operations.hpp>
+//#include <boost/thread/mutex.hpp>
 #include <boost/asio.hpp>
 #include <mutex>
 #include <string>
@@ -8,6 +8,8 @@
 #define SOCKSTATE_H
 
 //using namespace boost::asio;
+//using ip::tcp;
+
 
 namespace networking_util
 {
@@ -18,15 +20,16 @@ namespace networking_util
 
         public:
             // member variables
-            const boost::asio::ip::tcp::socket the_socket;
-            const int buffer_size;
-            const long id;
+            boost::asio::ip::tcp::socket *the_socket;
+            long id;
 
             // methods
-            socket_state(std::function<networking_util::socket_state> to_call, boost::asio::ip::tcp::socket s);
+            socket_state(std::function<networking_util::socket_state()> &to_call, boost::asio::ip::tcp::socket &s);
             std::function<networking_util::socket_state()> on_network_action;
-            std::string get_error_message;
-            bool get_error_occured;
+            std::string get_error_message() const;
+            bool get_error_occured() const;
+
+            std::string get_data();
             void remove_data(int start, int length);
             void clear_data();
 
@@ -35,9 +38,10 @@ namespace networking_util
             unsigned char buffer[4096];
             std::stringstream data;
             std::string error_message;
-
-            static long next_id;
+            bool error_occured;
+            long next_id; //used to be static
             std::mutex data_mutex;
     };
 }
+
 #endif
