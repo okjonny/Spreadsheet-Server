5c201a717cb7e16e39f7dbc02997654ea0ab26b1
jpalactor@gmail.com <jpalactor@gmail.com>
2021-04-13 00:19:19 +0000

    bro wtf

--- server_controller/socket_state.cpp
+++ server_controller/socket_state.cpp
@@ -1,5 +1,5 @@
 #include "socket_state.h"
-#include<boost/asio.hpp>
+#include <boost/asio.hpp>
 #include <mutex>
 
 #ifndef SOCKSTATE_H
@@ -9,31 +9,48 @@ using namespace boost::asio;
 
 namespace networking_util
 {
-    // ! Fix comments
-    class socket_state
+    friend class networking;
+
+    const tcp::socket the_socket = null;
+    const int buffer_size = 4096;
+    const long id;
+
+    socket_state::socket_state(std::function<socket_state> toCall, tcp::socket s)
     {
-        friend class networking;
-
-        public:
-            // member variables
-            const tcp::socket the_socket;
-            const int buffer_size;
-            const long id;
-
-            // methods
-            socket_state();
-            std::function<socket_state> on_network_action;
-            std::string get_error_message;
-            bool get_error_occured;
-            void remove_data(int start, int length);
-            void clear_data();
-
-        private:
-            // member variables
-            std::byte[] buffer;
-            std::basic_stringstream data;
-            std::string error_message;
-
-            static long next_id;
-            std::mutex data_mutex;
+        on_network_action = to_call;
+        the_socket = s;
+        id = next_id++;
     }
+
+    std::string socket_state::get_error_message()
+    {
+        return error_message;
+    }
+
+    bool socket_state::get_error_occured()
+    {
+        return error_occured;
+    }
+
+    std::string socket_state::get_data()
+    {
+        std::string retval;
+        {
+            std::lock_guard<std::mutex> guard(data_mutex);
+            // DO something here
+        }
+        return retval;
+    }
+
+    void socket_state::remove_data(int start, int length)
+    {
+    }
+
+    void socket_state::clear_data()
+    {
+
+    }
+
+    static long next_id = 0;
+    std::mutex data_mutex;
+}
