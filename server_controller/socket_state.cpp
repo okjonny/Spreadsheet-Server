e7fe2f2490cb0e1db48928036db49f9ea360ba71
jpalactor@gmail.com <jpalactor@gmail.com>
2021-04-13 02:36:33 +0000

    socket_state .cpp and .h stuff

--- server_controller/socket_state.cpp
+++ server_controller/socket_state.cpp
@@ -1,9 +1,8 @@
 #include "socket_state.h"
+#include <boost/filesystem/operations.hpp>
+#include <boost/thread/mutex.hpp>
 #include <boost/asio.hpp>
 #include <mutex>
-
-#ifndef SOCKSTATE_H
-#define SOCKSTATE_H
 
 using namespace boost::asio;
 
@@ -15,7 +14,7 @@ namespace networking_util
     const int buffer_size = 4096;
     const long id;
 
-    socket_state::socket_state(std::function<socket_state> toCall, tcp::socket s)
+    socket_state::socket_state(std::function<networking_util::socket_state> toCall, tcp::socket s)
     {
         on_network_action = to_call;
         the_socket = s;
@@ -44,6 +43,7 @@ namespace networking_util
 
     void socket_state::remove_data(int start, int length)
     {
+        // still need to implement
     }
 
     void socket_state::clear_data()
--- server_controller/socket_state.h
+++ server_controller/socket_state.h
@@ -1,8 +1,9 @@
-#include "socket_state.h"
-#include<boost/asio.hpp>
+#include <boost/filesystem/operations.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/asio.hpp>
 #include <mutex>
 
-#ifndef SOCKSTATE_H
+#ifndef SOCKSTATE_H 
 #define SOCKSTATE_H
 
 using namespace boost::asio;
@@ -16,28 +17,26 @@ namespace networking_util
 
         public:
             // member variables
-            const tcp::socket the_socket;
+            const ip::tcp::socket the_socket;
             const int buffer_size;
             const long id;
 
             // methods
             socket_state();
-            std::function<socket_state> on_network_action;
+            std::function<networking_util::socket_state> on_network_action;
             std::string get_error_message;
             bool get_error_occured;
             void remove_data(int start, int length);
             void clear_data();
 
         private:
-            //
             // member variables
-
-            std::byte[] buffer;
+            std::byte buffer [buffer_size];
             std::basic_stringstream data;
             std::string error_message;
 
             static long next_id;
             std::mutex data_mutex;
-
-    }
+    };
 }
+#endif
