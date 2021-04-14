e460cd5b2ad0a6babc39680820c39d65c4d72730
Jonny Palacios-Torres <jpalactor@gmail.com>
2021-04-14 07:25:54 +0000

    Added CMake files

---
+++ network_controller/CMakeFiles/tester.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tester.dir/networking_main.cpp.o"
+  "CMakeFiles/tester.dir/socket_state.cpp.o"
+  "tester"
+  "tester.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
