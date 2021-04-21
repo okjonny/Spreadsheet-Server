2a61dc63b89a4db1abe0d8eaa13fc5d71aa44bd0
jonny <jpalactor@gmail.com>
2021-04-21 19:38:27 +0000

    fuck

---
+++ network_server/CMakeFiles/server.o.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.o.dir/server.cpp.o"
+  "server.o"
+  "server.o.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.o.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
